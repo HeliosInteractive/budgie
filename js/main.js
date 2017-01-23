2d75c9585c80dcb4f6185ffba89dd33340228c68
Adam McFadden <adam.mcfadden@heliosinteractive.com>
2017-01-23 12:59:18 -0800

    working scroller that can go in any direction. Currently only supports images.

--- js/main.js
+++ js/main.js
@@ -9,34 +9,34 @@
 let testImages = ['http://placehold.it/350x150/660090','http://placehold.it/350x150/550099','http://placehold.it/350x150/440000','http://placehold.it/350x150/3300ff','http://placehold.it/350x150/2200ee','http://placehold.it/350x150/660000','http://placehold.it/350x150/1100bb','http://placehold.it/350x150/660000','http://placehold.it/350x150/770034','http://placehold.it/350x150/880000','http://placehold.it/350x150/9900f0','http://placehold.it/350x150/0f0011','http://placehold.it/350x150/800320','http://placehold.it/350x150/0670f1','http://placehold.it/350x150/362000']
 
 let infinity = function(images, selector, options = {}) {
+  images = options.inverted ? images.reverse() : images
+
   const defaultOptions = {
     'numberHigh': 1,
     'numberWide': 1,
     'clipOddEnding': false,
     'noScrollIfNoOverflow': true,
     'direction': 'vertical',
-    'inverted': false,
-    'secondsOnPage': 1.0
+    'secondsOnPage': 1.0,
+    'stopOnHover': false,
+    'inverted': false
   }
 
   options = Object.assign(defaultOptions, options)
 
   let ele = getElements(selector)[0]
+  ele.className += ' infinite-flex-container-parent'
+
   let scrollerDiv = document.createElement('div');
 
-  scrollerDiv.className += ' infinite-container-flex'
+  scrollerDiv.className += ' infinite-flex-container'
 
   ele.appendChild(scrollerDiv)
 
-  setFlexItemStyle(scrollerDiv, options)
-
-  console.log(images.length)
+  setScrollerCSS(scrollerDiv, options)
 
   if(options.clipOddEnding)
-    console.log(trimImagesArray(images, options).length)
     images = trimImagesArray(images, options)
-
-  console.log(images.length)
 
   images.forEach(function(image, index){
     scrollerDiv.appendChild(imageElement(image, index))
@@ -61,11 +61,20 @@ function trimImagesArray(images, options){
 }
 
 function appendFillerElements(scrollingElement, elementsOnScreen) {
-  let dupedElements = [].slice.call(getElements('.infinite-flex-item'), 0, elementsOnScreen);
+  let dupedElements;
+  if(options.inverted)
+    dupedElements = [].slice.call(getElements('.infinite-flex-item')).reverse().slice(0, elementsOnScreen);
+  else
+    dupedElements = [].slice.call(getElements('.infinite-flex-item'), 0, elementsOnScreen);
 
-  dupedElements.forEach(function(element){
-    scrollingElement.appendChild(element.cloneNode(true))
-  })
+  if(options.inverted)
+    dupedElements.forEach(function(element){
+      scrollingElement.insertBefore( element.cloneNode(true), scrollingElement.firstChild );
+    })
+  else
+    dupedElements.forEach(function(element){
+      scrollingElement.appendChild(element.cloneNode(true))
+    })
 }
 
 function measureScrollSection(imageCount, options) {
@@ -88,9 +97,9 @@ function elementMeasurement(options) {
 
 function viewPortMeasurement(options) {
   if(options.direction === 'vertical')
-    return parseFloat(window.getComputedStyle(getElements('.infinite-container-flex')[0]).height);
+    return parseFloat(window.getComputedStyle(getElements('.infinite-flex-container')[0]).height);
   else if(options.direction === 'horizontal')
-    return parseFloat(window.getComputedStyle(getElements('.infinite-container-flex')[0]).width);
+    return parseFloat(window.getComputedStyle(getElements('.infinite-flex-container')[0]).width);
   else
     throw new Error("Only vertical, and horizontal directions are accepted.")
 }
@@ -116,7 +125,8 @@ function imageElement(imgPath, index, options){
   return e;
 }
 
-function setFlexItemStyle(element, options){
+
+function setScrollerCSS(element, options){
   let eleWidth = parseInt(window.getComputedStyle(element).width);
   // let eleHeight = parseInt(window.getComputedStyle(element).height);
 
@@ -124,35 +134,53 @@ function setFlexItemStyle(element, options){
 
   // Width in %
   let width = ((eleWidth / options.numberWide / eleWidth) * 100);
-  // subtract 5% width to allow for flex to grow the space
-  // let width = width * 0.95
   let height = (100 / options.numberHigh);
 
   document.styleSheets[0].insertRule('.infinite-flex-item{width: ' + width + '%; height: ' + height + '%;}', numOfSheets);
+
+  let direction = options.direction === 'horizontal' ? 'column' : 'row'
+  document.styleSheets[0].insertRule('.infinite-flex-container{flex-direction: ' + direction + ';}', numOfSheets);
+
+  document.styleSheets[0].insertRule('.infinite-flex-container-parent{overflow: hidden;}', numOfSheets);
 }
 
 function startScroll(scrollingElement, scrollerSize, options) {
-  let topMargin = 0;
+  const fps = 60;
+
+  let currentMargin = options.inverted ? -scrollerSize : 0;
   let viewMeasure = viewPortMeasurement(options)
-  const fps = 30;
   let scrollSpeed = viewMeasure / options.secondsOnPage / fps
 
+  let marginSelector = {
+    'vertical':'marginTop',
+    'horizontal':'marginLeft'
+  }
+
+  console.log(marginSelector[options.direction])
+
+  console.log(viewMeasure)
+
   setInterval(function() {
-    scrollingElement.style.marginTop = (topMargin -= scrollSpeed) + 'px';
-    if(topMargin <= -scrollerSize)
-      topMargin = 0
-      scrollingElement.style.marginTop = topMargin + 'px';
+    let marginChange = options.inverted ? (currentMargin += scrollSpeed) : (currentMargin -= scrollSpeed)
+    scrollingElement.style[marginSelector[options.direction]] = marginChange + 'px';
+    if((!options.inverted && currentMargin <= -scrollerSize) || (options.inverted && currentMargin >= 0))
+      currentMargin = options.inverted ? -scrollerSize : 0;
+      scrollingElement.style[marginSelector[options.direction]] = currentMargin + 'px';
 
   }, 1000/fps)
 }
 
-infinity(testImages, '.main', {
-  'numberHigh': 3,
-  'numberWide': 3,
-  'clipOddEnding': true,
-  'secondsOnPage': 5.0
 
-})
+let options = {
+  'numberHigh': 2,
+  'numberWide': 2,
+  'clipOddEnding': true,
+  'secondsOnPage': 5.0,
+  'direction': 'horizontal',
+  'inverted': true
+}
+infinity(testImages, '.main', options)
+console.log(options)
 
 // To get the width of the flex-item in VW
 // =width/columns/width * 100
